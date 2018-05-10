03ab9fad91a8e26da5eb9fbbc8893e8d4da1200d
Kingdom0695 <kingdom0695@gmail.com>
2018-05-10 11:40:02 -0500

    Initial commit

---
+++ app/src/main/java/componentesfinal/example/omen/componentesfinal/PrincipalActivity.java
@@ -0,0 +1,13 @@
+package componentesfinal.example.omen.componentesfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
